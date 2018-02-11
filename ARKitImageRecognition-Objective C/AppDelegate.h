a1d454daa3e214ab6ee47066a0f4b40bcc96a722
Koushan Korouei <kkorouei@koushans-macbook-pro.local>
2018-02-11 08:01:43 +0330

    Initial Commit

---
+++ ARKitImageRecognition-Objective C/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ARKitImageRecognition-Objective C
+//
+//  Created by Koushan Korouei on 11/02/2018.
+//  Copyright © 2018 Koushan Korouei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ARKitImageRecognition-Objective C/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ARKitImageRecognition-Objective C
+//
+//  Created by Koushan Korouei on 11/02/2018.
+//  Copyright © 2018 Koushan Korouei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SceneKit/SceneKit.h>
+#import <ARKit/ARKit.h>
+
+@interface ViewController : UIViewController
+
+@end
